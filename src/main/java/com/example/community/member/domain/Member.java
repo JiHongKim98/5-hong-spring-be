4acdf2006e5e246aba8281600e0f15a2930da33f
JiHongKim98 <kinjihong9598@gmail.com>
2024-06-05 19:18:41 +0900

    feat: Add member entity and domain logics

---
+++ src/main/java/com/example/community/member/domain/Member.java
@@ -0,0 +1,77 @@
+package com.example.community.member.domain;
+
+import org.hibernate.annotations.SQLDelete;
+
+import com.example.community.common.domain.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// TODO: @SQLRestriction 을 제외 하려면 네이티브 쿼리 작성해야함 -> MySQL boolean 타입으로 캐스팅 불가 해결해야함
+@Getter
+@Entity
+@Table(name = "member")
+@SQLDelete(  // JPA DELETE 는 SOFT DELETE 로 설정
+	sql = "UPDATE member SET is_active = FALSE WHERE member_id = ?"
+)
+// @SQLRestriction("is_active = TRUE")  // hibernate 6.3 부터 `@Where` deprecated -> `@SQLRestriction`
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, unique = true)
+	private String nickname;
+
+	@Column(name = "profile_image")
+	private String profileImage;
+
+	@Column(name = "is_active")
+	private boolean isActive;
+
+	public boolean isMatchPassword(String password) {
+		return this.password.equals(password);
+	}
+
+	public void updatePassword(String password) {
+		this.password = password;
+	}
+
+	public void updateNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public void disabledMember() {
+		isActive = false;
+	}
+
+	public void enabledMember() {
+		isActive = true;
+	}
+
+	@Builder
+	public Member(String email, String password, String nickname, String profileImage) {
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.profileImage = profileImage;
+		this.isActive = true;
+	}
+}
---
+++ src/main/java/com/example/community/member/domain/respository/MemberRepository.java
@@ -0,0 +1,20 @@
+package com.example.community.member.domain.respository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.community.member.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	// TODO: N+1
+
+	boolean existsByEmail(String email);
+
+	boolean existsByNickname(String nickname);
+
+	Optional<Member> findByIdAndIsActiveTrue(Long memberId);
+
+	Optional<Member> findByEmailAndIsActiveTrue(String username);
+}
