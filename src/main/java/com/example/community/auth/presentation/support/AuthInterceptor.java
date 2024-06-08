ef19fd119d79cff914c7475b0b98478953d3b51d
JiHongKim98 <kinjihong9598@gmail.com>
2024-06-08 22:31:19 +0900

    refact: Simplify auth interceptor and extract methods

--- src/main/java/com/example/community/auth/presentation/support/AuthInterceptor.java
+++ src/main/java/com/example/community/auth/presentation/support/AuthInterceptor.java
@@ -1,5 +1,7 @@
 package com.example.community.auth.presentation.support;
 
+import static com.example.community.auth.exception.AuthExceptionType.*;
+
 import java.util.Objects;
 
 import org.springframework.stereotype.Component;
@@ -8,7 +10,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.example.community.auth.application.TokenExtractor;
 import com.example.community.auth.exception.AuthException;
-import com.example.community.auth.exception.AuthExceptionType;
 import com.example.community.common.annotation.LoginRequired;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,8 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class AuthInterceptor implements HandlerInterceptor {
 
-	// TODO: 리팩토링
-
 	private final AuthContext authContext;
 	private final TokenExtractor tokenExtractor;
 
@@ -32,16 +31,25 @@ public class AuthInterceptor implements HandlerInterceptor {
 		HttpServletResponse response,
 		Object handler
 	) throws Exception {
-		HandlerMethod handlerMethod = (HandlerMethod)handler;
-		LoginRequired annotation = handlerMethod.getMethodAnnotation(LoginRequired.class);
-		if (Objects.isNull(annotation)) {
-			return true;
+		if (isLoginRequired(handler)) {
+			String token = getTokenOrThrow(request);
+			Long memberId = tokenExtractor.extractAccessToken(token);
+			authContext.setContext(memberId);
 		}
-
-		String token = AuthHeaderExtractor.extract(request)
-			.orElseThrow(() -> new AuthException(AuthExceptionType.UNAUTHORIZED));
-		Long memberId = tokenExtractor.extractAccessToken(token);
-		authContext.setContext(memberId);
 		return true;
+	}
+
+	// @LoginRequired 어노테이션 검증
+	private boolean isLoginRequired(Object handler) {
+		if (handler instanceof HandlerMethod handlerMethod) {
+			LoginRequired annotation = handlerMethod.getMethodAnnotation(LoginRequired.class);
+			return Objects.nonNull(annotation);
+		}
+		return false;
+	}
+
+	private String getTokenOrThrow(HttpServletRequest request) {
+		return AuthHeaderExtractor.extract(request)
+			.orElseThrow(() -> new AuthException(UNAUTHORIZED));
 	}
 }
