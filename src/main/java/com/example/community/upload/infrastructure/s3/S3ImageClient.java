306a33ef3a2758c0c55050cb973ba0a8115846e4
JiHongKim98 <kinjihong9598@gmail.com>
2024-06-08 16:49:41 +0900

    feat: Add upload client and implements client with s3

---
+++ src/main/java/com/example/community/upload/infrastructure/s3/S3ImageClient.java
@@ -0,0 +1,30 @@
+package com.example.community.upload.infrastructure.s3;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.community.upload.application.ImageClient;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class S3ImageClient implements ImageClient {
+
+	// TODO: Async 기반으로 해야할듯 -> 이미지 저장 이벤트 발행
+
+	private final S3Uploader s3Uploader;
+	private final S3Properties properties;
+
+	@Override
+	public String upload(String objectKey, MultipartFile file) {
+		String bucketName = properties.bucketName();
+		s3Uploader.upload(bucketName, objectKey, file);
+		return generatedImageUrl(objectKey);
+	}
+
+	// image 가 저장된 URL 생성
+	private String generatedImageUrl(String objectKey) {
+		return properties.imageUrl() + objectKey;
+	}
+}
